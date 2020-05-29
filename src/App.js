71455ff22300faeb228717d16125efa332c98c32
elvis-onobo <elvis.onobo>
2020-05-29 12:39:46 +0100

    added forms and used state to display form values as they are submitted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			firstname: '',
+			lastname: ''
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	// this handleChange method will work for all all text based entries but not things like checkbox
+	handleChange(event) {
+		this.setState({
+			// event.target.name grabs the name of the form field e.g name="firstname"
+			// event.target.value grabs the value entered into the from 
+			[event.target.name]: event.target.value
+		})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<form>
+					<input type="text" name="firstname" placeholder="First Name" onChange={this.handleChange} />
+					<input type="text" name="lastname" placeholder="Last Name" onChange={this.handleChange} />
+					<p>{this.state.firstname} {this.state.lastname}</p>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default App;
