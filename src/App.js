538e8d76ece824f53872360c852ceb9698cdb94d
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-06-11 08:51:47 +0100

    added react router and changed anchor to Link

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Forgot from './components/auth/Forgot';
 import Reset from './components/auth/Reset';
+import Landing from './components/Landing';
+import NotFound from './components/NotFound';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Login />
-			<Register />
-			<Forgot />
-			<Reset />
+			<Switch>
+				<Route path='/' component={Landing} exact />
+				<Route path='/login' component={Login} />
+				<Route path='/register' component={Register} />
+				<Route path='/reset' component={Reset} />
+				<Route path='/register' component={Forgot} />
+				<Route component={NotFound} />
+			</Switch>
 			<Footer />
 		</div>
 	)
